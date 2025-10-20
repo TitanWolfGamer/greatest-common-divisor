f2078830245a8ba1b76326e0220968ce250d5716
TitanWolfCoder <99412625+TitanWolfGamer@users.noreply.github.com>
2025-10-20 10:28:36 +0200

    Fix typo in gcd function docstring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def gcd(a: int, b: int) -> int:
     """
-    gcd stands for "Greatest Common Divisor" and can compute that using the euclidian algorithm
+    gcd stands for "Greatest Common Divisor" and can compute that using the euclidean algorithm
     :param a: The first number
     :param b: The second number
     :return: The Greatest Common Divisor of these two numbers
