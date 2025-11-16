b27eb154f0f140398e23b161a47eae331666940a
TitanWolfCoder <99412625+TitanWolfGamer@users.noreply.github.com>
2025-11-16 16:31:38 +0100

    Improve gcd function and add example usage
    Refactor gcd function for clarity and add example usage.

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 def gcd(a: int, b: int) -> int:
     """
-    gcd stands for "Greatest Common Divisor" and can compute that using the euclidean algorithm
-    :param a: The first number
+    gcd stands for "Greatest Common Divisor" and can compute that using the Euclidean algorithm
+    :param a: the first number
     :param b: The second number
-    :return: The Greatest Common Divisor of these two numbers
+    :return: the greatest common divisor of the two given numbers
     """
-    remainder = a % b
-    while remainder:
+    r: int = a % b
+    while r:
         a = b
-        b = remainder
-        remainder = a % b
+        b = r
+        r = a % b
     return b
+
+
+# example usage
+def example() -> None:
+    num_a: int = int(input('Enter divisor: '))
+    num_b: int = int(input('Enter dividend: '))
+
+    gcd_of_ab: int = gcd(num_a, num_b)
+    print(f'GCD of both numbers: {gcd_of_ab}')
+
+    print(f'Your Input: {num_a}/{num_b}')
+    print(f'Your simplified input: {num_a // gcd_of_ab}/{num_b // gcd_of_ab}')
+    print(f'= {num_a / num_b}')
+
+if __name__ == '__main__':
+    example()
